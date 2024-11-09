3f5e183ec32e3fb991618b4370809fd33d9ebe8e
first <castrojonatan24@gmail.com>
2024-11-09 12:43:15 -0500

    Roller Coaster memoization version passes LocalTesting

--- SOLO puzzles/Roller Coaster/JAVA/LocalTesting/Main.java
+++ SOLO puzzles/Roller Coaster/JAVA/LocalTesting/Main.java
@@ -6,27 +6,45 @@ import java.math.*;
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
+class Pair {
+    int earnings;
+    int index;
+    Pair(int _earnings, int _index){
+        earnings = _earnings;
+        index = _index;
+    }
+}
+
 class Solution {
 
-    public static int loadRollerCoaster(Queue<Integer> groups, int C){
-        boolean isFull = false;
-        int currentCapacity = 0;
-        Queue<Integer> groupsOnRide = new LinkedList<>();
-        while(!isFull){
-            Integer nextGroup = groups.peek();
-            if(nextGroup == null || currentCapacity + nextGroup > C){
-                isFull = true;
-            } else {
-                nextGroup = groups.poll();
-                groupsOnRide.add(nextGroup);
-                currentCapacity += nextGroup;
+    public static Pair loadRollerCoaster(int index, int[] groups, int C, int[] indexEarningsMap, int[] fromIndexToIndexMap){
+
+        if(fromIndexToIndexMap[index] != -1 && indexEarningsMap[index] != -1){
+//            System.out.println("Memorized");
+            return new Pair(indexEarningsMap[index],fromIndexToIndexMap[index]);
+        } else {
+            // Calculate the toIndex and the earnings that have been made on this single ride
+            int fromIndex = index;
+            boolean isFull = false;
+            int currentCapacity = 0;
+            while (!isFull) {
+                if (index == groups.length || currentCapacity + groups[index] > C) {
+                    isFull = true;
+                } else {
+                    currentCapacity += groups[index];
+                    index++;
+                }
             }
+            int toIndex = index;
+            if(index == groups.length){
+                toIndex = 0;
+            }
+            int earnings = currentCapacity;
+            indexEarningsMap[fromIndex] = earnings;
+            fromIndexToIndexMap[fromIndex] = toIndex;
+            return new Pair(earnings, toIndex);
+
         }
-        while(!groupsOnRide.isEmpty()){
-            Integer group = groupsOnRide.poll();
-            groups.add(group);
-        }
-        return currentCapacity;
     }
 
     public static void main(String args[]) throws FileNotFoundException {
@@ -41,16 +59,25 @@ class Solution {
         int L = in.nextInt();
         int C = in.nextInt();
         int N = in.nextInt();
-        Queue<Integer> groups = new LinkedList<>();
+        int[] groups = new int[N];
+        int[] indexEarningsMap = new int[N];
+        int[] fromIndexToIndexMap = new int[N];
         for (int i = 0; i < N; i++) {
             int pi = in.nextInt();
-            groups.add(pi);
+            groups[i] = pi;
+            indexEarningsMap[i] = -1;
+            fromIndexToIndexMap[i] = -1;
         }
         int numberOfRides = 0;
         BigInteger earnings = new BigInteger("0");
+        int index = 0;
         while(numberOfRides < C){
-            BigInteger singleRideEarnings = BigInteger.valueOf(loadRollerCoaster(groups,L));
+            Pair rideResult = loadRollerCoaster(index, groups, L, indexEarningsMap, fromIndexToIndexMap);
+
+            BigInteger singleRideEarnings = BigInteger.valueOf(rideResult.earnings);
             earnings = earnings.add(singleRideEarnings);
+            index = rideResult.index;
+
             numberOfRides++;
         }
         
